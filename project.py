9ea8489d1d0d1ea141b689774fecaa41ecb62494
Yahirzyr <123759878+Yahirzyr@users.noreply.github.com>
2023-01-27 17:54:31 +0000

    added project 1

---
+++ project.py
@@ -0,0 +1,7 @@
+#but your int into a letter
+b = int(input("Input the base : "))
+h = int(input("Input the height : "))
+#now times tour letters and devide
+area = b*h/2
+#print your area
+print("area = ", area)
